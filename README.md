1341ecf16e1c548d74bf273b7e17ca71610f1e8d
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-12-07 17:10:42 +0100

    MAKE A PUBLIC INTERFACE FOR PURE RECURS. METHOD
    We have implemented not two, but three possible solutions to the PROBLEM
    1. The iterative method self.fibs(num) uses just an iterative block.
    I'ts time complexity is O(n)
    
    2. The recursive method self.fibs_rec(num) uses a uses an recursive private function to calculate the Fibbonaci number,
    but an iterative block to store it in the matrix.
    It's code maybe is the most clear, but also the least efficient, because use an recursive method nested
    in a iterative loop (I think its an O(n * log n) method)
    
    3. We have also made a pure recursive method self.fibs_pure_rec(num) to solve the problem.
    self.fibs_pure_rec(num) is just a public interface to protect the true (private) pure recursive method:
    self.fibs_pure_rec(num, counter, response)
    This method uses three arguments: the number, the counter and the response array
    The number is given by user thought self.fibs_pure_rec(num)
    The other two arguments ara just for recursive internal use
    I'ts time complexity is O(n)

--- README.md
+++ README.md
@@ -19,3 +19,25 @@ A significant part of computer science is dedicated to sorting data. An algorith
 
 Merge sort is one such sorting algorithm, and can be much faster than other algorithms such as bubble sort on the right data sets. Essentially merge sort recurses through an array of unsorted data until it reaches its smallest sub-set, a single item. Of course an array with a single item is considered sorted. Merge sort then merges the single items back together in sorted order. Pretty clever!
 
+#SOLUTIONS EXPLANATION:
+
+SOLVE THE FIBONACCI PROBLEM
+
+We have implemented not two, but three possible solutions to the PROBLEM
+1. The iterative method self.fibs(num) uses just an iterative block. 
+I'ts time complexity is O(n)
+
+2. The recursive method self.fibs_rec(num) uses a uses an recursive private function to calculate the Fibbonaci number,
+but an iterative block to store it in the matrix.
+It's code maybe is the most clear, but also the least efficient, because use an recursive method nested
+in a iterative loop (I think its an O(n * log n) method)
+
+3. We have also made a pure recursive method self.fibs_pure_rec(num) to solve the problem.
+self.fibs_pure_rec(num) is just a public interface to protect the true (private) pure recursive method:
+self.fibs_pure_rec(num, counter, response)
+This method uses three arguments: the number, the counter and the response array
+The number is given by user thought self.fibs_pure_rec(num)
+The other two arguments ara just for recursive internal use
+I'ts time complexity is O(n)
+
+
--- lib/Fibonacci.rb
+++ lib/Fibonacci.rb
@@ -22,7 +22,40 @@ class Fibonacci
   
   # This method uses pure recursion, without any iterative block
   # The problem is if the user calls this method with more than 1 argument, the calculation breaks
-  def self.fibs_pure_rec(num, counter = 1, response = [])
+
+
+  # The recursive method uses an recursive private function to calculate the Fibbonaci number,
+  # but an iterative block to store it in the matrix
+  def self.fibs_rec(num)
+    response=[]
+    num = 1 if num < 1
+    (1..num).each {|i| response << self.fibs_calculate(i)}
+    response
+  end
+
+  # This method is just an interface of the private self.fibs_pure_rec_calc(num) method,
+  # which is the TRULY pure iterative method.
+  # This interface is needed to protect self.fibs_pure_rec_calc(num) from an incorrect use
+  def self.fibs_pure_rec(num)
+    self.fibs_pure_rec_calc(num, 1, [])
+  end
+
+  private 
+
+  # This iterative method is used by self.fibs_rec(num) method
+  def self.fibs_calculate(num)
+    if num <= 1
+      0
+    elsif num == 2
+      1
+    else
+      self.fibs_calculate(num-1) + self.fibs_calculate(num-2)
+    end
+  end
+
+  # This is the TRULY pure recursive method
+  # Is protected by the public interface method self.fibs_pure_rec(num)
+  def self.fibs_pure_rec_calc(num, counter, response)
     num = 1 if num < 1
     if counter == 1
       response << 0
@@ -35,28 +68,7 @@ class Fibonacci
       #End of recursivity; we ofer the response
       response
     else
-      self.fibs_pure_rec(num, counter + 1, response)
-    end
-  end
-
-  # The recursive method uses an recursive private function to calculate the Fibbonaci number,
-  # but an iterative block to store it in the matrix
-  def self.fibs_rec(num)
-    response=[]
-    num = 1 if num < 1
-    (1..num).each {|i| response << self.fibs_calculate(i)}
-    response
-  end
-
-  private 
-
-  def self.fibs_calculate(num)
-    if num <= 1
-      0
-    elsif num == 2
-      1
-    else
-      self.fibs_calculate(num-1) + self.fibs_calculate(num-2)
+      self.fibs_pure_rec_calc(num, counter + 1, response)
     end
   end
 
